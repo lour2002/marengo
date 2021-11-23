62aab46074d6be73c19dd115f897ce68d6d29c58
Borys <boris.avs@live.ru>
2021-11-23 23:13:36 +0200

    Create footer html on main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,7 +11,35 @@
         <!-- Styles -->
     </head>
     <body>
-
+        <header></header>
+        <main></main>
+        <footer>
+            <div>
+                <div>
+                    <!-- TODO: Fields [link, title] -->
+                    <a href="/" title="Положения и словия">Положения и словия</a>
+                    <!-- TODO: Fields [link, title] -->
+                    <a href="/" title="Политика кондефициальности">Политика кондефициальности</a>
+                    <!-- TODO: Fields [link, title] -->
+                    <a href="/" title="Политика Cookies">Политика Cookies</a>
+                </div>
+                <img src="" width="163" height="58" alt="Marengo">
+                <div>
+                    <!-- TODO: Fields [link, title, icon] -->
+                    <a href="/" title="Facebook">
+                        <img src="" width="31" height="31" alt="Facebook">
+                    </a>
+                    <!-- TODO: Fields [link, title, icon] -->
+                    <a href="/" title="Instagram">
+                        <img src="" width="31" height="31" alt="Instagram">
+                    </a>
+                    <!-- TODO: Fields [link, title, icon] -->
+                    <a href="/" title="Youtube">
+                        <img src="" width="45" height="32" alt="Youtube">
+                    </a>
+                </div>
+            </div>
+        </footer>
     <!-- Scripts -->
     </body>
 </html>
