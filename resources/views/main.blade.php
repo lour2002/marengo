56c2316386c91c65169afe1d765a2d7515695866
Borys <boris.avs@live.ru>
2021-11-24 22:29:13 +0200

    [Main page] html for header and section 4

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,19 +11,44 @@
         <!-- Styles -->
     </head>
     <body>
-        <header></header>
-        <main></main>
-        <footer>
+        <header>
+            {{-- Desktop --}}
             <div>
                 <div>
-                    <!-- TODO: Fields [link, title] -->
-                    <a href="/" title="Положения и словия">Положения и словия</a>
-                    <!-- TODO: Fields [link, title] -->
-                    <a href="/" title="Политика кондефициальности">Политика кондефициальности</a>
-                    <!-- TODO: Fields [link, title] -->
-                    <a href="/" title="Политика Cookies">Политика Cookies</a>
+                    <a href="#">Продукція</a>
+                    <a href="#">Коктейлі</a>
+                    <a href="#">Історія</a>
                 </div>
-                <img src="" width="163" height="58" alt="Marengo">
+                <img src="" width="204" height="72" alt="Marengo">
+                <div>
+                    <button type="button">Вермути</button>
+                    <button type="button">Ігристі</button>
+                </div>
+            </div>
+            <div>
+                <select>
+                    <option value="uk">Укр</option>
+                    <option value="ru">Рос</option>
+                    <option value="en">Англ</option>
+                </select>
+            </div>
+            {{-- Mobile --}}
+            <button type="button"></button>
+            <div>
+                <div>
+                    <button type="button">Вермути</button>
+                    <button type="button">Ігристі</button>
+                </div>
+                <div>
+                    <a href="#">Продукція</a>
+                    <a href="#">Коктейлі</a>
+                    <a href="#">Історія</a>
+                </div>
+                <ul>
+                    <li data-value="uk">Укр</li>
+                    <li data-value="ru">Рос</li>
+                    <li data-value="en">Англ</li>
+                </ul>
                 <div>
                     <!-- TODO: Fields [link, title, icon] -->
                     <a href="/" title="Facebook">
@@ -36,6 +61,51 @@
                     <!-- TODO: Fields [link, title, icon] -->
                     <a href="/" title="Youtube">
                         <img src="" width="45" height="32" alt="Youtube">
+                    </a>
+                </div>
+            </div>
+        </header>
+        <main>
+            <section></section>
+            <section></section>
+            <section></section>
+
+            <section>
+                <div>
+                    <!-- TODO: Fields [title] -->
+                    <h2>Історія MARENGO</h2>
+                    <!-- TODO: Fields [text] -->
+                    <p>История нашего бренда началась в 2001 году, когда была разлита первая бутылка Marengo на винзаводе Коблево. Сама идея создания высококачественного вермута появилась с момента основания завода в 1982 году.  Именно поэтому на символике Marengo выбита эта цифра.</p>
+                    <p>Основным ингредиентом нашего вермута является виноград, из которого мы производим вино высочайшего качества для приготовления Marengo. Наши технологи долго экспериментировали с различными сортами европейского винограда и разнообразным сочетанием трав и пряностей, чтобы получить сбалансированный вкус и аромат, которым вы наслаждаетесь. </p>
+                    <p>История нашего бренда началась в 2001 году, когда была разлита первая бутылка Marengo на винзаводе Коблево. Сама идея создания высококачественного вермута появилась с момента основания завода в 1982 году.  Именно поэтому на символике Marengo выбита эта цифра.</p>
+                </div>
+                <!-- TODO: Fields [] -->
+                <img src="" alt="Пейзаж" width="100%" load="lazy">
+            </section>
+        </main>
+        <footer>
+            <div>
+                <div>
+                    <!-- TODO: Fields [link, title] -->
+                    <a href="/" title="Положення та умови">Положення та умови</a>
+                    <!-- TODO: Fields [link, title] -->
+                    <a href="/" title="Політика кондефіційності">Політика кондефіційності</a>
+                    <!-- TODO: Fields [link, title] -->
+                    <a href="/" title="Політика Cookies">Політика Cookies</a>
+                </div>
+                <img src="" width="163" height="58" alt="Marengo">
+                <div>
+                    <!-- TODO: Fields [link, title, icon] -->
+                    <a href="/" title="Facebook">
+                        <img src="" load="lazy" width="45" height="32" alt="Facebook">
+                    </a>
+                    <!-- TODO: Fields [link, title, icon] -->
+                    <a href="/" title="Instagram">
+                        <img src="" load="lazy" width="45" height="32" alt="Instagram">
+                    </a>
+                    <!-- TODO: Fields [link, title, icon] -->
+                    <a href="/" title="Youtube">
+                        <img src="" load="lazy" width="45" height="32" alt="Youtube">
                     </a>
                 </div>
             </div>
